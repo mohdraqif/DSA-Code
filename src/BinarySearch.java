a19fd771600564107f87cf7e108e4622a6d77ed9
mohdraqif <mohdraqif94@gmail.com>
2025-03-23 12:32:09 +0530

    Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+  // O(log n)
+  static int binarySearch(int arr[], int key) {
+    int start = 0, end = arr.length-1;
+
+    while(start <= end) {
+      int mid = (start+end)/2;
+      if(key == arr[mid]) return mid; 
+      if(arr[mid] < key) start = mid + 1;
+      else end = mid - 1;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int numbers[] = {3,7,2,4,4,9,22,5,6};
+    int foundIndex = binarySearch(numbers, 22);
+    if(foundIndex == -1) {
+      System.out.println("Key not present in the array");
+    } else {
+      System.out.println("Element found at index: " + foundIndex);
+    }
+  }
+}
